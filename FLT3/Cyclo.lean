4e4d38c194d21bd705c8023ed19a09c7bb79f81c
Pietro Monticone <38562595+pitmonticone@users.noreply.github.com>
2024-04-07 17:14:56 +0200

    Update Cyclo.lean

--- FLT3/Cyclo.lean
+++ FLT3/Cyclo.lean
@@ -1,7 +1,7 @@
 /-
 Copyright (c) 2024 Riccardo Brasca. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
-Authors: Riccardo Brasca
+Authors: Riccardo Brasca, Pietro Monticone
 -/
 
 import Mathlib.NumberTheory.Cyclotomic.Embeddings
