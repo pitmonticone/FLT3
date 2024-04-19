3371afc81abf5ea839698bfa84de0691f5c0ec4f
Pietro Monticone <38562595+pitmonticone@users.noreply.github.com>
2024-04-20 01:14:03 +0200

    update

--- FLT3/FLT3.lean
+++ FLT3/FLT3.lean
@@ -984,18 +984,12 @@ lemma final : S.Y ^ 3 + (S.u₄ * S.Z) ^ 3 = S.u₅ * (λ ^ (S.multiplicity - 1)
   rw [show S.Y ^ 3 + (S.u₄ * S.Z) ^ 3 = S.Y ^ 3 + S.u₄^2 * S.u₄ * S.Z ^ 3 by ring]
   have f2 := formula2 S
   rw [show Y S ^ 3 + ↑(u₄ S) * Z S ^ 3 = Y S ^ 3 + 1 * ↑(u₄ S) * Z S ^ 3 by ring] at f2
-  suffices hyp : S.u₄ = (1 : 𝓞 K)  ∨ S.u₄ = (-1 : 𝓞 K) by
-    have hh : S.u₄ ^ 2 = (1 : 𝓞 K) := by
-      rcases hyp with (h | h)
-      · rw [h]
-        simp only [one_pow]
-      · rw [h]
-        simp only [even_two, Even.neg_pow, one_pow]
-    nth_rewrite 1 [← hh] at f2
-    exact f2
   have simple_kummer := by_kummer S
   simp only [Finset.mem_insert, Finset.mem_singleton] at simple_kummer
-  exact simple_kummer
+  have hh : S.u₄ ^ 2 = (1 : 𝓞 K) := by
+    rcases simple_kummer with (h | h) <;> simp only [h, one_pow, even_two, Even.neg_pow, one_pow]
+  nth_rewrite 1 [← hh] at f2
+  exact f2
 
 /-- Given `S : Solution`, we can construct a `Solution'_final : Solution'`. -/
 noncomputable
